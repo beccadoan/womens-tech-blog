8b9664ab82b14c44b1729fbc1ccd64c71166fc0b
Becca Doan <rebecca.doan@yahoo.com>
2022-08-10 21:29:41 -0500

    added routes for categories, profiles, and favorites

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const homeRoutes = require('./home-routes.js')
+const profileRoutes = require('./profile-routes.js')
+const categoryRoutes = require('./category-routes.js')
+const favoriteRoutes = require('./favorite-routes.js')
 const router = require('express').Router();
 
 router.use('/', homeRoutes)
-// router.use('/api', apiRoutes);
+router.use('/profiles', profileRoutes)
+router.use('/categories', categoryRoutes)
+router.use('/favorites', favoriteRoutes)
+router.use('/api', apiRoutes);
+
 module.exports = router;
