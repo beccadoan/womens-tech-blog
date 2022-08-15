8309500fb4126872126db872d61fe5dd25680614
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-08-15 18:47:43 -0500

    updated mailer

--- public/javascript/mailer.js
+++ public/javascript/mailer.js
@@ -1,27 +1,31 @@
 const nodemailer = require('nodemailer');
 
-async function main() {
-    let testAccount = await
-    nodemailer.createTestAccount();
+require('dotenv').config()
 
+async function main() {
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
+        host: "smtp.office365.com",
         port: 587,
         secure: false,
         auth: {
-            user: testAccount.user,
-            pass: testAccount.pass
+            user: "womenstechblog@outlook.com",
+            pass: process.env.OU_PW
         },
+        tls: {
+            rejectUnauthorized: false
+        }
     });
 
     let info = await transporter.sendMail({
-        from: '"Cait" <cait@example.com>',
-        to: "becca@example.com",
-        subject: "Hello",
-        text: "Hello world"
+        from: '"Womens Tech Blog" <womenstechblog@outlook.com>',
+        to: "womenstechblog@gmail.com",
+        subject: "Contact request",
+        text: "Contact form submitted",
+        html: "<h3>Name: ${name}</h3>"
     });
 
     console.log('Message sent', info.messageId);
 }
 
 main().catch(console.error);
+document.querySelector('.contact-btn').addEventListener('submit', main);
