b4efe526b05952bc5237cb15ff174a59b0ca3373
Becca Doan <rebecca.doan@yahoo.com>
2022-08-17 23:35:07 -0500

    styling

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const categories = require('./categories')
+
 module.exports = {
     format_date: date => {
       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
@@ -12,7 +14,7 @@ module.exports = {
       return word;
     },
 
-    getUserID: () => {
-      return req.session.user_id
+    getCategories: () => {
+      return categories
     }
 };
